585cd6715bcd95e6ff8d9f5c01e9f945dd416635
Orhideous <orhideous@gmail.com>
2014-06-24 15:25:51 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description="Pure Python 3 JSON-RPC 2.0 transport realisation",
     long_description=read('README.rst'),
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
